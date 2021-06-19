7fc9b3c3e6765341be72b24274799aa8774dbbb1
Marcos <mp2772@gmail.com>
2021-06-19 18:07:14 -0400

    review for functions and arrays lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,24 +1,69 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(a, b) {
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(wordArray) {
+  let longestWord = ``;
+  if(wordArray.length === 0) {
+    return null;
+  }
+
+  for(let i = 0; i < wordArray.length; i++) {
+    if(wordArray[i].length > longestWord.length) {
+      longestWord = wordArray[i];
+    }
+  }
+
+  return longestWord;
+}
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbersArray) {
+  let sum = 0; // 0 + 6 = 6 + 12 = 18 + 1 = 19 + 18 = 37
+  for(let i = 0; i < numbersArray.length; i++) {
+    sum += numbersArray[i]; // 0 + 6 = 6 + 12 = 18 + 1 = 19 + 18 = 37
+  }
+
+  return sum;
+}
 
 
 
 // Iteration #3.1 Bonus:
-function sum() {}
+function sum(numArray) {
+  let result = 0;
+  if(sum.length === 0) {
+    return 0;
+  }
+
+  for(let i = 0; i < numArray.length; i++) {
+    if (Array.isArray(numArray[i]) || typeof numArray[i] === 'object') {
+      throw new Error("Unsupported data type sir or ma'am");
+    } else if (typeof numArray[i] === 'boolean') {
+      result += Number(numArray[i]);
+    } else if (typeof numArray[i] === 'string') {
+      result += numArray[i].length;
+    } else {
+      result += numArray[i];
+    }
+  }
+
+  return result;
+}
 
 
 
@@ -26,13 +71,37 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(oneArray) {
+  let sumTotal = 0;
+  if(oneArray.length === 0) {
+    return null;
+  }
+
+  for(let i = 0; i < oneArray.length; i++) {
+    sumTotal += oneArray[i];
+  }
+    
+  return sumTotal / oneArray.length;
+
+  // return sumNumbers(oneArray) / oneArray.length; <==> since sumNumbers function already returns sum of numbers array. It can be called to get the sum then you just have to divide by the length of oneArray to get the average.
+}
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(arrOfWords) {
+  let wordSum = 0;
+  if(arrOfWords.length === 0) {
+    return null;
+  }
+  
+  for(let i = 0; i < arrOfWords.length; i++) {
+    wordSum += arrOfWords[i].length;
+  }
+
+  return wordSum / arrOfWords.length;
+}
 
 // Bonus - Iteration #4.1
 function avg() {}
